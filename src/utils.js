edf7a53f45dbb8197097143c9f191f8473b79ba3
ScottishCyclops <scottlandart@gmail.com>
2018-12-13 23:05:07 +0100

    Moved methods and added comments

--- src/utils.js
+++ src/utils.js
@@ -1,39 +1,64 @@
 const crypto = require('crypto')
+const moment = require('moment')
 
+/**
+ * Output a list of strings as a log to the stdout
+ *
+ * @param  {...string} args the strings to output
+ * @returns {void} nothing
+ * @unpure
+ */
 function log(...args)
 {
-  console.log(...args)
-}
-
-function warn(...args)
-{
-  console.warn(...args)
-}
-
-function err(...args)
-{
-  console.error(...args)
+  process.stdout.write(`[?] ${args.join(' ')}`)
 }
 
 /**
+ * Output a list of strings as a warning to the stdout
+ *
+ * @param  {...string} args the strings to output
+ * @returns {void} nothing
+ * @unpure
+ */
+function warn(...args)
+{
+  process.stdout.write(`[!] ${args.join(' ')}`)
+}
+
+/**
+ * Output a list of strings as an error to the stderr
+ *
+ * @param  {...string} args the strings to output
+ * @returns {void} nothing
+ * @unpure
+ */
+function err(...args)
+{
+  process.stderr.write(`[x] ${args.join(' ')}`)
+}
+
+/**
+ * Assert an expression is true. If not, exit the program with an optional code and error message
  *
  * @param {boolean} x the expression to assert
- * @param {number} code the exit code in case of failure
- * @param  {...string} error the error message in case of failure
+ * @param {number} code the optional exit code in case of failure
+ * @param  {...string} error the optional error message in case of failure
  * @returns {never | void} never returns in case of failure, or returns nothing
+ * @unpure
  */
 function assert(x, code, ...error)
 {
-  if (x) return
+  if (Boolean(x) === true) return
 
-  err(...error)
-  process.exit(code)
+  if (error.length > 0) err(...error)
+
+  process.exit(code || 1)
 }
 
 /**
  * Get the MD5 hash of the given strings
  *
- * @param {...string} strings a list of stringd
+ * @param {...string} strings a list of strings
  * @returns {string} an hexadecimal hash string
  * @pure
  */
@@ -44,10 +69,23 @@ function md5Hash(...strings)
   return md5sum.digest('hex')
 }
 
+/**
+ * Turn a time string of format "00:00.00" into a number of milliseconds
+ *
+ * @param {string} timeString the time string
+ * @returns {number} the number of milliseconds
+ * @pure
+ */
+function parseTimeString(timeString)
+{
+  return moment.duration(`00:${timeString}`).asMilliseconds()
+}
+
 module.exports = {
   log,
   warn,
   err,
   assert,
-  md5Hash
+  md5Hash,
+  parseTimeString
 }
